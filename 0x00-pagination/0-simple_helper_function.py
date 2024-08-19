68b375ee3a8819a24fee6ff158cd58e513eab664
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-08-19 19:35:48 +0100

    type annotate variables

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,6 +7,7 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
   """return page start and end index"""
-  start_index = page_size * (page - 1)
-  end_index = page_size * page
-  return start_index, end_index
+  start_index: int = page_size * (page - 1)
+  end_index: int = page_size * page
+
+  return (start_index, end_index)
