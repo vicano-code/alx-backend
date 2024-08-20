1f59e9bb878e242dbe9d63b1995373a30bc6e280
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-08-20 13:44:43 +0100

    function documentation

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -27,6 +27,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns a page of lists given the page and page size"""
         # Validate input type and value
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
