4377a605c2530fbb1bcc44b9e0846c0fe5b973ca
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-08-21 19:13:41 +0100

    restructure code

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,8 +10,9 @@ class BasicCache(BaseCaching):
     """
     def put(self, key, item):
         """put data in cache"""
-        if key or item is not None:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
         """get cache data"""
